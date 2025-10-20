0d679825afb6eafbbfa89762e0bdb231e60dbb58
Winamu6 <29032006art@mail.ru>
2025-10-21 00:07:03 +0300

    add writer service and update reader service

--- order_service/src/services/reader_service.py
+++ order_service/src/services/reader_service.py
@@ -1,24 +1,31 @@
-from typing import List
+from typing import List, Optional
 
 from order_service.src.repository import ReadRepository
 from order_service.src.schemas import OrderRead
 
-class ProductReader:
+
+class OrderReader:
     def __init__(self, repository: ReadRepository):
         self.repo = repository
 
-    async def get_order_by_id(self, product_id: int) -> OrderRead | None:
-        product = await self.repo.get_order_by_id(product_id)
-        return OrderRead.from_orm(product) if product else None
+    async def get_order_by_id(self, order_id: int) -> Optional[OrderRead]:
+        order = await self.repo.get_order_by_id(order_id)
+        return OrderRead.from_orm(order) if order else None
 
-    async def get_list_all_orders_for_users(self, user_id: int,limit: int = 100, offset: int = 0) -> List[OrderRead]:
-        orders = await self.repo.list_all_orders_for_user(user_id ,limit, offset)
+    async def get_all_orders_for_user(
+        self, user_id: int, limit: int = 100, offset: int = 0
+    ) -> List[OrderRead]:
+        orders = await self.repo.list_all_orders_for_user(user_id, limit, offset)
         return [OrderRead.from_orm(o) for o in orders]
 
-    async def get_list_active_orders_for_users(self, user_id: int,limit: int = 100, offset: int = 0) -> List[OrderRead]:
-        orders = await self.repo.list_active_orders_for_user(user_id ,limit, offset)
+    async def get_active_orders_for_user(
+        self, user_id: int, limit: int = 100, offset: int = 0
+    ) -> List[OrderRead]:
+        orders = await self.repo.list_active_orders_for_user(user_id, limit, offset)
         return [OrderRead.from_orm(o) for o in orders]
 
-    async def get_sort_order_by_status_for_user(self, user_id: int,limit: int = 100, offset: int = 0) -> List[OrderRead]:
-        orders = await self.repo.sort_orders_by_status_for_user(user_id ,limit, offset)
+    async def get_sorted_orders_by_status_for_user(
+        self, user_id: int, limit: int = 100, offset: int = 0
+    ) -> List[OrderRead]:
+        orders = await self.repo.sort_orders_by_status_for_user(user_id, limit, offset)
         return [OrderRead.from_orm(o) for o in orders]
