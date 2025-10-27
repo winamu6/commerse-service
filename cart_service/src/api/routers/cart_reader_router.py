3f5445b23ccb6a4c29a759027d171a8b9840a7af
Winamu6 <29032006art@mail.ru>
2025-10-27 15:00:22 +0300

    add cart read router

--- cart_service/src/api/routers/cart_reader_router.py
+++ cart_service/src/api/routers/cart_reader_router.py
@@ -2,8 +2,18 @@ from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException, Query
 
-from cart_service.src.api.depends import get_reader_service
+from cart_service.src.api.depends import get_cart_read_service
 from cart_service.src.schemas import CartResponse
 from cart_service.src.services.read_service import CartReadService
 
 router = APIRouter(prefix="/cart_read", tags=["CartRead"])
+
+@router.get("/{user_id}/cart", response_model=List[CartResponse])
+async def get_cart(
+    user_id: int,
+    service: CartReadService = Depends(get_cart_read_service),
+):
+    cart = await service.get_cart(user_id)
+    if not cart:
+        raise HTTPException(status_code=404, detail="Cart not found")
+    return cart
