432fc4787d0d6d5fa5e0ab06ca49b8242cd8388f
Winamu6 <29032006art@mail.ru>
2025-10-27 14:44:25 +0300

    add read depends

---
+++ cart_service/src/api/depends/read_depends.py
@@ -0,0 +1,21 @@
+from fastapi import Depends
+from cart_service.src.db.cache import redis_client
+from cart_service.src.services.cache_service import CartCache
+from cart_service.src.repository.read_repository import CartReadRepository
+from cart_service.src.services.read_service import CartReadService
+
+
+async def get_cache_service() -> CartCache:
+    yield CartCache(redis_client)
+
+
+async def get_cart_read_repository(
+    cache_service: CartCache = Depends(get_cache_service),
+) -> CartReadRepository:
+    yield CartReadRepository(cache_service)
+
+
+async def get_cart_read_service(
+    repository: CartReadRepository = Depends(get_cart_read_repository),
+) -> CartReadService:
+    yield CartReadService(repository)
