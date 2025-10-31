d8fd13db606e7a5671ffaf9950b52e7a369c9840
Winamu6 <29032006art@mail.ru>
2025-11-01 00:36:59 +0300

    add write repository

---
+++ payment_service/src/repository/__init__.py
@@ -0,0 +1,4 @@
+from .read_repository import ReadRepository
+from .write_repository import WriterRepository
+
+__all__ = ["ReadRepository", "WriterRepository"]
--- payment_service/src/repository/write_repository.py
+++ payment_service/src/repository/write_repository.py
@@ -1,10 +1,35 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from typing import Optional, Tuple, List
-
 from payment_service.src.models import Payment, PaymentStatus
 
 
 class WriterRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+    async def create_payment(self, payment: Payment) -> Payment:
+        self.session.add(payment)
+        try:
+            await self.session.commit()
+            await self.session.refresh(payment)
+        except Exception as e:
+            await self.session.rollback()
+            raise e
+        return payment
+
+    async def update_payment(self, payment_id: int, update_data: dict) -> Payment | None:
+        payment = await self.session.get(Payment, payment_id)
+        if not payment:
+            return None
+
+        for key, value in update_data.items():
+            if hasattr(payment, key):
+                setattr(payment, key, value)
+
+        try:
+            await self.session.commit()
+            await self.session.refresh(payment)
+        except Exception as e:
+            await self.session.rollback()
+            raise e
+
+        return payment
