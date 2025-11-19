95ad97f510e674c411a5d6670eeca1f665a15581
Winamu6 <29032006art@mail.ru>
2025-11-19 09:20:25 +0300

    api init commit

---
+++ payment_service/src/api/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+from payment_service.src.api.routers.payment_reader_router import router as payment_reader_router
+from payment_service.src.api.routers.payment_writer_router import router as payment_writer_router
+
+app = FastAPI(title='payment_service_api')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(payment_reader_router, prefix='/api')
+app.include_router(payment_writer_router, prefix='/api')
+
+if __name__ == '__main__':
+    uvicorn.run(
+        app=app,
+        host='0.0.0.0',
+        port=8001,
+    )
